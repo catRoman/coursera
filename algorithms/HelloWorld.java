ab4c550fb560647a0c482f7c88b609ae3ae37f1a
catRoman <cjroman@lakeheadu.ca>
2021-01-24 09:27:59 -0800

    staged all new modifications-Laptop-fast push -a

---
+++ algorithms/HelloWorld.java
@@ -0,0 +1,8 @@
+/** Coursera Course Algorithms by Wayne and Sedgewick
+* Exercise 1
+*/
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello, World");
+	}
+}
